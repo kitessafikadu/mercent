9bca89c6adc1b60e710b1ca3707f846a4639364f
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-09 11:46:18 +0300

    Update schema.prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SubCategoriesModule } from './subcategories/subcategories.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { SubCategoriesModule } from './subcategories/subcategories.module';
     ProductsModule,
     CategoriesModule,
     SubCategoriesModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
